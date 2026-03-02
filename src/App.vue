e88694143a8f774e31eab69de5eb0223c79e9dca
Angela Ramos <avramosl@academia.usbbog.edu.co>
2026-03-02 03:49:36 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div class="container">
+    <h1>PERSONAJES</h1>
+    <h1>Rick and Morty</h1>
+
+    <div class="grid">
+      <CharacterCard
+        v-for="character in characters"
+        :key="character.id"
+        :character="character"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CharacterCard from './assets/components/CharacterCard.vue'
+
+export default {
+  components: { CharacterCard },
+  data() {
+    return {
+      characters: []
+    }
+  },
+  mounted() {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(res => res.json())
+      .then(data => {
+        this.characters = data.results
+      })
+      .catch(error => {
+        console.error('Error al consumir la API:', error)
+      })
+  }
+}
+</script>
+
---
+++ src/assets/components/CharacterCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <img :src="character.image" :alt="character.name" />
+    <h3>{{ character.name }}</h3>
+    <p>Especie: {{ character.species }}</p>
+    <p>Estado: {{ character.status }}</p>
+
+    <button @click="mostrarInfo">Ver información completa</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    character: Object
+  },
+  methods: {
+    mostrarInfo() {
+      alert(JSON.stringify(this.character, null, 2))
+    }
+  }
+}
+</script>
